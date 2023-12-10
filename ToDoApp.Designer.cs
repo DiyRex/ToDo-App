f20a53bdcbc2d5f928f8cf556eb0ac70938a0546
DiyRex <dsdissanayaka2002@gmail.com>
2023-12-10 16:18:52 +0530

    Added Add Task Button

--- ToDoApp.Designer.cs
+++ ToDoApp.Designer.cs
@@ -29,26 +29,59 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.txtTask = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(157, 21);
+            this.label1.Location = new System.Drawing.Point(204, 22);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(138, 29);
             this.label1.TabIndex = 0;
             this.label1.Text = "To Do App";
             // 
-            // Form1
+            // txtTask
+            // 
+            this.txtTask.Location = new System.Drawing.Point(247, 111);
+            this.txtTask.Name = "txtTask";
+            this.txtTask.Size = new System.Drawing.Size(133, 20);
+            this.txtTask.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(181, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Task :";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(128, 177);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Add Task";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // ToDoApp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(446, 459);
+            this.ClientSize = new System.Drawing.Size(542, 542);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTask);
             this.Controls.Add(this.label1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "ToDoApp";
+            this.Text = "To Do App";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -57,6 +90,9 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTask;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAdd;
     }
 }
 
--- ToDoApp.cs
+++ ToDoApp.cs
@@ -16,5 +16,10 @@ namespace ToDoApp
         {
             InitializeComponent();
         }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+          
+        }
     }
 }
